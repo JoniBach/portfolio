ec45afdc1ad85e9f60151f2f563e300292980776
JoniBach <48724197+JoniBach@users.noreply.github.com>
2022-03-24 14:52:58 +0000

    Deploying to gh-pages from @ JoniBach/portfolio@8e89c6fa6424bfdc3111ae07f2076fbd9d809f24 🚀

---
+++ _next/static/lZy9uoZemLcODuELhyNfv/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/659-fcad3526b008eea3.js","static/chunks/pages/index-64440442d561f924.js"],"/_error":["static/chunks/pages/_error-6679c1efb053c50e.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
